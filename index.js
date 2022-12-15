71a62c54b0670fa3ef5432cf141edcafffdd061e
sleon <sleon008@ikasle.ehu.eus>
2022-12-15 21:26:37 +0100

    .

--- index.js
+++ index.js
@@ -2,29 +2,18 @@ const session = require('express-session')
 const express = require("express");
 const app = express()
 
-const PORT = 3000;
+const PORT = 4000;
+const indexRouter= require('./routes/index');
 
 const sess = {
     secret: 'ausazko hitz multzoa',
-    cookie: {}
+    cookie: {maxAge:20000}
 }
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.use(session(sess))
-
-
-// Access the session as req.session
-app.get('/', function(req, res, next) {
-    if (req.session.views) {
-        req.session.views++
-        res.setHeader('Content-Type', 'text/html')
-        res.write('<p>views: ' + req.session.views + '</p>')
-        res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
-        res.end()
-    } else {
-        req.session.views = 1
-        res.end('welcome to the session demo. refresh!')
-    }
-})
+app.use('/', indexRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
